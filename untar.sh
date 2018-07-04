78e36903b6a87230eb4602f1ad4e134d6cba7dfd
Sergey Muha <muchasergey@gmail.com>
2018-07-04 10:46:18 +0300

    Tests

--- untar.sh
+++ untar.sh
@@ -4,8 +4,8 @@ ls -ltr
 pwd
 tar -zxvf dora-droplet.tar.gz
 
-cp -a app/* ../files2/
-cp staging_info.yml ../files2
+cp -a app/* ../output/
+cp staging_info.yml ../output
 ls -ltr
 
-ls -la ../files2
+ls -la ../output
