e4d56e6073e7f30ab4638cb527c9fe6457c0150e
Sergey Muha <muchasergey@gmail.com>
2018-07-04 14:06:46 +0300

    asd

--- untar.sh
+++ untar.sh
@@ -4,7 +4,14 @@ ls -la ..
 ls -ltr
 pwd
 tar -zxf dora-droplet-new.tar.gz -C ../output/
-sed -i 's/ruby/ruby_buildpack/' ../output/staging_info.yml
+#sed -i 's/ruby/ruby_buildpack/' ../output/staging_info.yml
+cat <<EOF > ../output/app/manifest.yml
+---
+applications:
+- name: my-app
+  memory: 512M
+  instances: 1
+EOF
 cat ../output/staging_info.yml
 #cp -a app/* ../output/
 #cp -a * ../output/
