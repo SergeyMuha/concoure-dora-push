f1425fbf1ef8eda111c8301d041629fd2fd09df0
Sergey Muha <muchasergey@gmail.com>
2018-07-04 13:38:06 +0300

    asd

--- untar.sh
+++ untar.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+rm -rf pipeline.yml
 ls -la ..
 ls -ltr
 pwd
-tar -zxvf dora-droplet-new.tar.gz
+tar -zxvf dora-droplet-new.tar.gz -C ../output/
 
-cp -a app/* ../output/
-cp -a * ../output/
-cp staging_info.yml ../output
+#cp -a app/* ../output/
+#cp -a * ../output/
+#cp staging_info.yml ../output
 ls -ltr
 
 ls -la ../output
