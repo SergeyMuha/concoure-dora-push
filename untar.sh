6d9da310163081944a1199245fb29fa84629b03d
Sergey Muha <muchasergey@gmail.com>
2018-07-04 13:52:59 +0300

    asd

--- untar.sh
+++ untar.sh
@@ -3,7 +3,7 @@ rm -rf pipeline.yml
 ls -la ..
 ls -ltr
 pwd
-tar -zxvf dora-droplet-new.tar.gz -C ../output/
+tar -zxf dora-droplet-new.tar.gz -C ../output/
 sed -i 's/ruby/ruby_buildpack/' ../output/staging_info.yml
 cat ../output/staging_info.yml
 #cp -a app/* ../output/
